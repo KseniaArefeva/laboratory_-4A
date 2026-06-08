1aade5ba75f55332d2c623d02e2dfa3c1bf82b24
KseniaArefeva <150457796+KseniaArefeva@users.noreply.github.com>
2026-06-08 08:49:18 +0300

    Update code.py

--- code.py
+++ code.py
@@ -13,7 +13,6 @@ text = input("Введите текст: ")
 freq = word_frequency(text)
 
 print("\nРЕЗУЛЬТАТ:")
-print("-" * 35)
 for word, count in sorted(freq.items(), key=lambda x: x[1], reverse=True):
     print(f"  '{word}': {count}")
 print(f"\nВсего уникальных слов: {len(freq)}")
